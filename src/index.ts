694f10f8c90145ae7d84c35e87d5d8f5d7479f07
Valentina <korolenkovalya0704@gmail.com>
2023-10-09 13:07:48 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+// Вам потрібно створити умовний тип, що служить для встановлення типу, що повертається з функції. 
+// Як параметр типу повинен обов'язково виступати функціональний тип.
+
+type ReturnTypeFunction<T> = T extends (...args: any[]) => infer R ? R : never;
+
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+type Add = ReturnTypeFunction<typeof add>  
+
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+type Greet = ReturnTypeFunction<typeof greet> 
+
+
+
+// Вам потрібно створити умовний тип, який приймає функціональний тип 
+// з одним параметром (або задовільним) та повертає кортеж,
+//  де перше значення - це тип, що функція повертає, а другий - тип її параметру
+
+
+// type FunctionInfo<T> = T extends (param: infer P) => infer R ? [R, P] : never;
+type FunctionInfo<T> = T extends (param: infer ParamType) => infer ReturnType ? [ReturnType, ParamType]: never;
+
+function greetInfo(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+type GreetInfo = FunctionInfo<typeof greet>;
+
+
+function multiply(a: number): number {
+  return a * 5;
+}
+
+type MultiplyInfo = FunctionInfo<typeof multiply>;
+
+
+function exampleFunction(y: string): boolean {
+  return y.length > 7;
+}
+
+type Info = FunctionInfo<typeof exampleFunction>
+
+
+
+
